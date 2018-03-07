bd58c33dc681cbee4a26a52002333988235591d5
ashif <ashif@bras>
2018-03-07 12:36:19 +0600

    Added: Equals, Hash and toString methods in CommentReply.java model

--- src/main/java/com/uiu/thesis/models/forum/CommentReply.java
+++ src/main/java/com/uiu/thesis/models/forum/CommentReply.java
@@ -8,6 +8,7 @@ package com.uiu.thesis.models.forum;
 import com.uiu.thesis.models.user.HumanResource;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -50,5 +51,107 @@ public class CommentReply implements Serializable {
      * Constructor
      */
     public CommentReply() {
+    }
+
+    /**
+     * Getter and setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public HumanResource getReplier() {
+        return replier;
+    }
+
+    public void setReplier(HumanResource replier) {
+        this.replier = replier;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    /**
+     * hash method
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        hash = 17 * hash + Objects.hashCode(this.reply);
+        hash = 17 * hash + Objects.hashCode(this.dateTime);
+        hash = 17 * hash + Objects.hashCode(this.replier);
+        hash = 17 * hash + Objects.hashCode(this.comment);
+        return hash;
+    }
+
+    /**
+     * equals method
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CommentReply other = (CommentReply) obj;
+        if (!Objects.equals(this.reply, other.reply)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.dateTime, other.dateTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.replier, other.replier)) {
+            return false;
+        }
+        if (!Objects.equals(this.comment, other.comment)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentReply{" + "id=" + id + ", reply=" + reply + ", dateTime=" + dateTime + ", replier=" + replier + ", comment=" + comment + '}';
     }
 }
