8f6df07ce163369af07173c34a575de83be110c7
ashif <ashif@bras>
2018-03-07 13:16:06 +0600

    Added: Equals, Hash and toString methods in OfficeResource.java and OfficeResourceType.java

--- src/main/java/com/uiu/thesis/models/object_resource/OfficeResource.java
+++ src/main/java/com/uiu/thesis/models/object_resource/OfficeResource.java
@@ -6,6 +6,7 @@
 package com.uiu.thesis.models.object_resource;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -48,4 +49,110 @@ public class OfficeResource implements Serializable {
     public OfficeResource() {
     }
 
+    /**
+     * Getter and setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public OfficeResourceType getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(OfficeResourceType resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    /**
+     * Hash Method
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.room);
+        hash = 37 * hash + Objects.hashCode(this.floor);
+        hash = 37 * hash + this.quantity;
+        hash = 37 * hash + Objects.hashCode(this.resourceType);
+        return hash;
+    }
+
+    /**
+     * Equals method
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OfficeResource other = (OfficeResource) obj;
+        if (this.quantity != other.quantity) {
+            return false;
+        }
+        if (!Objects.equals(this.room, other.room)) {
+            return false;
+        }
+        if (!Objects.equals(this.floor, other.floor)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.resourceType, other.resourceType)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * To string method
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "OfficeResource{" + "id=" + id + ", room=" + room + ", floor=" + floor + ", quantity=" + quantity + ", resourceType=" + resourceType + '}';
+    }
 }
--- src/main/java/com/uiu/thesis/models/object_resource/OfficeResourceType.java
+++ src/main/java/com/uiu/thesis/models/object_resource/OfficeResourceType.java
@@ -7,6 +7,7 @@ package com.uiu.thesis.models.object_resource;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -41,6 +42,73 @@ public class OfficeResourceType implements Serializable {
      * Constructor
      */
     public OfficeResourceType() {
+    }
+
+    /**
+     * Getter and Setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public List<OfficeResource> getOfficeResources() {
+        return officeResources;
+    }
+
+    public void setOfficeResources(List<OfficeResource> officeResources) {
+        this.officeResources = officeResources;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.resourceType);
+        hash = 37 * hash + Objects.hashCode(this.officeResources);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OfficeResourceType other = (OfficeResourceType) obj;
+        if (!Objects.equals(this.resourceType, other.resourceType)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.officeResources, other.officeResources)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "OfficeResourceType{" + "id=" + id + ", resourceType=" + resourceType + ", officeResources=" + officeResources + '}';
     }
 
 }
