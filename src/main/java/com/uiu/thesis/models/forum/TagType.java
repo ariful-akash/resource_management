e45544e5235f473edccae359551925451d4eb71a
ashif <ashif@bras>
2018-03-07 12:47:08 +0600

    Added: Equals, Hash and toString methods in TagType.java model

--- src/main/java/com/uiu/thesis/models/forum/TagType.java
+++ src/main/java/com/uiu/thesis/models/forum/TagType.java
@@ -7,6 +7,7 @@ package com.uiu.thesis.models.forum;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -42,4 +43,86 @@ public class TagType implements Serializable {
     public TagType() {
     }
 
+    /**
+     * Getter and setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    /**
+     * Hash method
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + Objects.hashCode(this.id);
+        hash = 47 * hash + Objects.hashCode(this.tag);
+        hash = 47 * hash + Objects.hashCode(this.posts);
+        return hash;
+    }
+
+    /**
+     * Equals method
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TagType other = (TagType) obj;
+        if (!Objects.equals(this.tag, other.tag)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.posts, other.posts)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * To string method
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "TagType{" + "id=" + id + ", tag=" + tag + ", posts=" + posts + '}';
+    }
 }
