bdce7473719fd2eccf6d2fc0e4c7d79e809b99d0
ashif <ashif@bras>
2018-03-11 17:17:16 +0600

    Added: RequisitionType.java model with fields and their getter-setter

---
+++ src/main/java/com/uiu/thesis/models/requisition/RequisitionType.java
@@ -0,0 +1,102 @@
+package com.uiu.thesis.models.requisition;
+
+import com.uiu.thesis.models.user.AccessType;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ashif
+ */
+@Entity
+@Table(name = "requisition_types")
+public class RequisitionType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @OneToOne
+    private AccessType accessType;
+
+    /**
+     * Constructor
+     */
+    public RequisitionType() {
+    }
+
+    /**
+     * Getter and setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public AccessType getAccessType() {
+        return accessType;
+    }
+
+    public void setAccessType(AccessType accessType) {
+        this.accessType = accessType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.type);
+        hash = 53 * hash + Objects.hashCode(this.accessType);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RequisitionType other = (RequisitionType) obj;
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.accessType, other.accessType)) {
+            return false;
+        }
+        return true;
+    }
+
+}
