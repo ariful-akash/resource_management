1fbb8760043564bc66853f9e2b962ce1c23c0173
ashif <ashif@bras>
2018-03-12 16:41:02 +0600

    Edited: Mapping between HumanResource.java model

--- src/main/java/com/uiu/thesis/models/requisition/Requisition.java
+++ src/main/java/com/uiu/thesis/models/requisition/Requisition.java
@@ -1,11 +1,8 @@
 package com.uiu.thesis.models.requisition;
 
-import com.uiu.thesis.models.forum.TagType;
 import com.uiu.thesis.models.user.HumanResource;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,8 +10,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -56,14 +51,13 @@ public class Requisition implements Serializable {
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private HumanResource solver;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "requisition_creator_id")
     private HumanResource requisitionCreator;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(name = "requisition_tag_types", joinColumns = {
-        @JoinColumn(name = "requisition_id")}, inverseJoinColumns = {
-        @JoinColumn(name = "tag_id")})
-    private List<TagType> tags;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "requisition_type_id")
+    private RequisitionType requisitionType;
 
     /**
      * Constructor
@@ -71,154 +65,4 @@ public class Requisition implements Serializable {
     public Requisition() {
     }
 
-    /**
-     * Getter and Setter
-     *
-     * @return
-     */
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public Date getRequisitionNeedDate() {
-        return requisitionNeedDate;
-    }
-
-    public void setRequisitionNeedDate(Date requisitionNeedDate) {
-        this.requisitionNeedDate = requisitionNeedDate;
-    }
-
-    public Date getRequisitionPlacingDate() {
-        return requisitionPlacingDate;
-    }
-
-    public void setRequisitionPlacingDate(Date requisitionPlacingDate) {
-        this.requisitionPlacingDate = requisitionPlacingDate;
-    }
-
-    public String getPurpose() {
-        return purpose;
-    }
-
-    public void setPurpose(String purpose) {
-        this.purpose = purpose;
-    }
-
-    public String getRemarks() {
-        return remarks;
-    }
-
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
-    public boolean isIsSolved() {
-        return isSolved;
-    }
-
-    public void setIsSolved(boolean isSolved) {
-        this.isSolved = isSolved;
-    }
-
-    public HumanResource getSolver() {
-        return solver;
-    }
-
-    public void setSolver(HumanResource solver) {
-        this.solver = solver;
-    }
-
-    public HumanResource getRequisitionCreator() {
-        return requisitionCreator;
-    }
-
-    public void setRequisitionCreator(HumanResource requisitionCreator) {
-        this.requisitionCreator = requisitionCreator;
-    }
-
-    public List<TagType> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<TagType> tags) {
-        this.tags = tags;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 43 * hash + Objects.hashCode(this.id);
-        hash = 43 * hash + this.quantity;
-        hash = 43 * hash + Objects.hashCode(this.requisitionNeedDate);
-        hash = 43 * hash + Objects.hashCode(this.requisitionPlacingDate);
-        hash = 43 * hash + Objects.hashCode(this.purpose);
-        hash = 43 * hash + Objects.hashCode(this.remarks);
-        hash = 43 * hash + (this.isSolved ? 1 : 0);
-        hash = 43 * hash + Objects.hashCode(this.solver);
-        hash = 43 * hash + Objects.hashCode(this.requisitionCreator);
-        hash = 43 * hash + Objects.hashCode(this.tags);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Requisition other = (Requisition) obj;
-        if (this.quantity != other.quantity) {
-            return false;
-        }
-        if (this.isSolved != other.isSolved) {
-            return false;
-        }
-        if (!Objects.equals(this.purpose, other.purpose)) {
-            return false;
-        }
-        if (!Objects.equals(this.remarks, other.remarks)) {
-            return false;
-        }
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        if (!Objects.equals(this.requisitionNeedDate, other.requisitionNeedDate)) {
-            return false;
-        }
-        if (!Objects.equals(this.requisitionPlacingDate, other.requisitionPlacingDate)) {
-            return false;
-        }
-        if (!Objects.equals(this.solver, other.solver)) {
-            return false;
-        }
-        if (!Objects.equals(this.requisitionCreator, other.requisitionCreator)) {
-            return false;
-        }
-        if (!Objects.equals(this.tags, other.tags)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Requisition{" + "id=" + id + ", quantity=" + quantity + ", requisitionNeedDate=" + requisitionNeedDate + ", requisitionPlacingDate=" + requisitionPlacingDate + ", purpose=" + purpose + ", remarks=" + remarks + ", isSolved=" + isSolved + ", solver=" + solver + ", requisitionCreator=" + requisitionCreator + ", tags=" + tags + '}';
-    }
 }
--- src/main/java/com/uiu/thesis/models/requisition/RequisitionType.java
+++ src/main/java/com/uiu/thesis/models/requisition/RequisitionType.java
@@ -2,12 +2,15 @@ package com.uiu.thesis.models.requisition;
 
 import com.uiu.thesis.models.user.AccessType;
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -28,75 +31,16 @@ public class RequisitionType implements Serializable {
     @Column(name = "type", nullable = false)
     private String type;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "access_type_id")
     private AccessType accessType;
+
+    @OneToMany(mappedBy = "requisitionType", cascade = CascadeType.ALL)
+    private List<Requisition> requisitions;
 
     /**
      * Constructor
      */
     public RequisitionType() {
     }
-
-    /**
-     * Getter and setter
-     *
-     * @return
-     */
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public AccessType getAccessType() {
-        return accessType;
-    }
-
-    public void setAccessType(AccessType accessType) {
-        this.accessType = accessType;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 53 * hash + Objects.hashCode(this.id);
-        hash = 53 * hash + Objects.hashCode(this.type);
-        hash = 53 * hash + Objects.hashCode(this.accessType);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final RequisitionType other = (RequisitionType) obj;
-        if (!Objects.equals(this.type, other.type)) {
-            return false;
-        }
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        if (!Objects.equals(this.accessType, other.accessType)) {
-            return false;
-        }
-        return true;
-    }
-
 }
