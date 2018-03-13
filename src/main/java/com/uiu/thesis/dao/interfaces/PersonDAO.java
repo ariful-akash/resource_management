f00eb70448a8bf31a64c41a66f67281821baaa1c
ashif <ashif@bras>
2018-03-13 16:53:18 +0600

    CHANGED: Created separate package for DAO interfaces and implementations. "com.uiu.thesis.dao.interfaces" package for dao interfaces and "com.uiu.thesis.dao.implement" for dao implemented classes.

--- src/main/java/com/uiu/thesis/dao/interfaces/PersonDAO.java
+++ src/main/java/com/uiu/thesis/dao/interfaces/PersonDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.uiu.thesis.dao;
+package com.uiu.thesis.dao.interfaces;
 
 import com.uiu.thesis.models.Person;
 
