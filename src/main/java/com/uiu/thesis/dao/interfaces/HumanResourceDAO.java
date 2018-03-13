dca050eced04e638beaa963b9b145f5a6890462e
ashif <ashif@bras>
2018-03-14 00:32:11 +0600

    ADDED: interface methods are added in HumanResourceDAO.java

--- src/main/java/com/uiu/thesis/dao/interfaces/HumanResourceDAO.java
+++ src/main/java/com/uiu/thesis/dao/interfaces/HumanResourceDAO.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.uiu.thesis.dao.interfaces;
+
+import com.uiu.thesis.models.user.HumanResource;
+import com.uiu.thesis.models.user.HumanResourceType;
+import com.uiu.thesis.models.user.Role;
+import java.util.List;
 
 /**
  *
@@ -11,4 +11,17 @@ package com.uiu.thesis.dao.interfaces;
  */
 public interface HumanResourceDAO {
 
+    public boolean addHumanResource(HumanResource hr);
+
+    public boolean updateHumanResource(HumanResource oldHr, HumanResource newHr);
+
+    public boolean deleteHumanResource(HumanResource hr);
+
+    public HumanResource getHumanResource(Long id);
+
+    public List<HumanResource> getAllHumanResources();
+
+    public List<HumanResource> getHumanResources(HumanResourceType hrType);
+
+    public List<HumanResource> getHumanResources(Role role);
 }
