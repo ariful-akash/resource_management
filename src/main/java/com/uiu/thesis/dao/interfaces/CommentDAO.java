1b8a2015da7371399549c3334e0e10ee2215b27a
ashif <ashif@bras>
2018-03-14 11:09:10 +0600

    Added: Added interface methods in CommentDAO.java

--- src/main/java/com/uiu/thesis/dao/interfaces/CommentDAO.java
+++ src/main/java/com/uiu/thesis/dao/interfaces/CommentDAO.java
@@ -5,10 +5,29 @@
  */
 package com.uiu.thesis.dao.interfaces;
 
+import com.uiu.thesis.models.forum.Comment;
+import com.uiu.thesis.models.forum.Post;
+import java.util.List;
+
 /**
  *
  * @author ashif
  */
 public interface CommentDAO {
 
+    public boolean addComment(Post post, Comment comment);
+
+    public boolean addComment(Long postId, Comment comment);
+
+    public boolean editComment(Long oldId, String newContent);
+
+    public boolean deleteComment(Comment comment);
+
+    public boolean deleteComment(Long commentId);
+
+    public List<Comment> getAllComments(Post post);
+
+    public List<Comment> getAllComments(Long postId);
+
+    public Comment getComment(Long commentId);
 }
