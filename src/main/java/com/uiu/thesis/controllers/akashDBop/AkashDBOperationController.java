a1e287a4cd1054e6d6005a2e20c94c4a4ae49a14
Ariful Islam Akash <ariful.uiu.cse@gmail.com>
2018-03-20 00:56:23 +0600

    IMPLEMENTED: addOfficeResourceType() method is implemented
    ADDED: addORType() controller method (url: /insert/ortype) to insert dummy Office Resource Type Data in "office_resource_types" table in DB

---
+++ src/main/java/com/uiu/thesis/controllers/akashDBop/AkashDBOperationController.java
@@ -0,0 +1,74 @@
+package com.uiu.thesis.controllers.akashDBop;
+
+import com.uiu.thesis.dao.interfaces.OfficeResourceTypeDAO;
+import com.uiu.thesis.models.object_resource.OfficeResourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ariful
+ */
+@Controller
+
+public class AkashDBOperationController {
+
+    @Autowired
+    private OfficeResourceTypeDAO orTypeDAO;
+
+    /**
+     * insert OfficeResourceType table
+     *
+     * @return
+     */
+    @RequestMapping(value = "/insert/ortype")
+    public String addORType() {
+
+        String orTypes[] = {
+            "class room chair",
+            "canteen chair",
+            "faculty chair",
+            "study chair (study room, library)",
+            "special chair",
+            "waiting chair",
+            "guest chair",
+            "class room table",
+            "canteen table",
+            "faculty table",
+            "study table",
+            "special table",
+            "presentation table",
+            "projector table",
+            "computer lab table",
+            "cisco lab table",
+            "digital lab table",
+            "projector",
+            "monitor",
+            "CPU",
+            "faculty shelf",
+            "almirah",
+            "server",
+            "multiplug",
+            "book shelf",
+            "mouse",
+            "keyboard",
+            "printer",
+            "photocopier",
+            "scaner"
+        };
+        int id = 0;
+        OfficeResourceType orType = new OfficeResourceType();
+        for (String type : orTypes) {
+
+            orType.setResourceType(type);
+            id = orTypeDAO.addOfficeResourceType(orType);
+        }
+
+        if (id != 0) {
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
+}
