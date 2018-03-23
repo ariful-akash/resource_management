798b92d6b6bc7252f829c6542fc0dea3c6fc6b8c
ashif <ashif@bras>
2018-03-23 10:37:24 +0600

    TESTED: Update is working perfectly.

--- src/main/java/com/uiu/thesis/controllers/ashifDBop/AshifDBOperationController.java
+++ src/main/java/com/uiu/thesis/controllers/ashifDBop/AshifDBOperationController.java
@@ -5,8 +5,10 @@ import com.uiu.thesis.dao.interfaces.HumanResourceDAO;
 import com.uiu.thesis.dao.interfaces.RoleDAO;
 import com.uiu.thesis.models.user.AccessType;
 import com.uiu.thesis.models.user.Role;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -143,6 +145,58 @@ public class AshifDBOperationController {
 
             Role role = (Role) roleIterator.next();
             System.out.println("ID: " + role.getId() + ", Role: " + role.getRole());
+        }
+
+        return "success";
+    }
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "/map/roleaccesstype")
+    public String mapAccessRole() {
+
+        List<Role> roles = roleDAO.getAllRoles();
+        List<AccessType> allAccessTypes = accessTypeDAO.getAllAccessTypes();
+
+        Role role;
+        Set<AccessType> roleSpecificAccessTypes = new HashSet<>();
+
+        Iterator rolesIterator = roles.iterator();
+
+        while (rolesIterator.hasNext()) {
+
+            role = (Role) rolesIterator.next();
+
+            switch (role.getId().intValue()) {
+
+                case 1:
+
+                    System.out.println(allAccessTypes.get(0).getDescription());
+
+                    roleSpecificAccessTypes.add(allAccessTypes.get(0));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(1));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(2));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(3));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(4));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(5));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(6));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(11));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(12));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(17));
+                    roleSpecificAccessTypes.add(allAccessTypes.get(18));
+
+                    role.setAccessTypes(roleSpecificAccessTypes);
+
+                    roleDAO.updateRole(role);
+
+                    roleSpecificAccessTypes.clear();
+
+                    break;
+                case 2:
+                    break;
+            }
         }
 
         return "success";
