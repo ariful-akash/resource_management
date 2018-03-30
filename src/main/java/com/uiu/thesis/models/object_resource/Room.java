328f045e7f05b3c1e2c7e8924e42ba20e7cf6dee
ashif <ashif@bras>
2018-03-31 01:59:28 +0600

    ADDED: Floor.java and Room.java model classes attributes, mapping, getter-setter, equals hash method, toString method is added.

--- src/main/java/com/uiu/thesis/models/object_resource/Room.java
+++ src/main/java/com/uiu/thesis/models/object_resource/Room.java
@@ -1,9 +1,27 @@
 package com.uiu.thesis.models.object_resource;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author ashif
  */
-public class Room {
+@Entity
+@Table(name = "room")
+public class Room implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "room")
+    private String room;
 }
