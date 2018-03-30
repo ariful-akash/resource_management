131333b9a03b0b94c049e2b2cf0633419155e6fe
ashif <ashif@bras>
2018-03-31 02:22:36 +0600

    IMPLEMENTED: addCommentReply(CommentReply commentReply) method is implemented.

--- src/main/java/com/uiu/thesis/dao/implement/CommentReplyDAOImpl.java
+++ src/main/java/com/uiu/thesis/dao/implement/CommentReplyDAOImpl.java
@@ -5,6 +5,9 @@ import com.uiu.thesis.models.forum.Comment;
 import com.uiu.thesis.models.forum.CommentReply;
 import java.util.List;
 import javax.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,9 +18,21 @@ import org.springframework.stereotype.Repository;
 @Transactional
 public class CommentReplyDAOImpl implements CommentReplyDAO {
 
+    @Autowired(required = true)
+    private SessionFactory sessionFactory;
+
+    /**
+     *
+     * @param commentReply
+     * @return
+     */
     @Override
     public int addCommentReply(CommentReply commentReply) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        Session session = sessionFactory.getCurrentSession();
+        Long id = (Long) session.save(commentReply);
+
+        return Integer.valueOf(id.toString());
     }
 
     @Override
