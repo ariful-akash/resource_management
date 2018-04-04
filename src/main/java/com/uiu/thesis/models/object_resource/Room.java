12bd9d21b6cacd81889f8dadd1bfac7258d75e18
ashif <ashif@bras>
2018-04-05 00:29:17 +0600

    ADDED: A field floorId is added to trace floor. Constructor, getters & setters, equals and hash method, toString method is added.

--- src/main/java/com/uiu/thesis/models/object_resource/Room.java
+++ src/main/java/com/uiu/thesis/models/object_resource/Room.java
@@ -1,6 +1,7 @@
 package com.uiu.thesis.models.object_resource;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,4 +25,81 @@ public class Room implements Serializable {
 
     @Column(name = "room")
     private String room;
+
+    @Column(name = "floor_id")
+    private Long floorId;
+
+    /**
+     * Constructor
+     */
+    public Room() {
+    }
+
+    /**
+     * Getter and Setter
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Long getFloorId() {
+        return floorId;
+    }
+
+    public void setFloorId(Long floorId) {
+        this.floorId = floorId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        hash = 41 * hash + Objects.hashCode(this.room);
+        hash = 41 * hash + Objects.hashCode(this.floorId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        if (!Objects.equals(this.room, other.room)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.floorId, other.floorId)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" + "id=" + id + ", room=" + room + ", floorId=" + floorId + '}';
+    }
+
 }
