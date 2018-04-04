86cf72defc1751285e35c097ec2a38bef2f03ce6
ashif <ashif@bras>
2018-04-05 00:40:21 +0600

    ADDED: FloorDAOImpl class is added that implements FloorDAO interface.

---
+++ src/main/java/com/uiu/thesis/dao/implement/FloorDAOImpl.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uiu.thesis.dao.implement;
+
+import com.uiu.thesis.dao.interfaces.FloorDAO;
+import com.uiu.thesis.models.object_resource.Floor;
+import java.util.List;
+
+/**
+ *
+ * @author ashif
+ */
+public class FloorDAOImpl implements FloorDAO {
+
+    @Override
+    public int addFloor(Floor floor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateFloor(Floor floor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Floor> getAllFloor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
