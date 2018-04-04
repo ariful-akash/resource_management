5f83bac371fcf2c434a6db6e469c65df325945fc
ashif <ashif@bras>
2018-04-05 00:30:09 +0600

    ADDED: FloorDAO interface is added.

---
+++ src/main/java/com/uiu/thesis/dao/interfaces/FloorDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uiu.thesis.dao.interfaces;
+
+import com.uiu.thesis.models.object_resource.Floor;
+import java.util.List;
+
+/**
+ *
+ * @author ashif
+ */
+public interface FloorDAO {
+
+    public int addFloor(Floor floor);
+
+    public int updateFloor(Floor floor);
+
+    public List<Floor> getAllFloor();
+}
