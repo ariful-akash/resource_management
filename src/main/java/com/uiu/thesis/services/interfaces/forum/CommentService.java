c207fe0bae183c698f4e096bab786da9bf5dcde9
ashif <ashif@bras>
2018-04-06 13:21:10 +0600

    ADDED; CommentService.java interface is added to provide service for Comment class.

---
+++ src/main/java/com/uiu/thesis/services/interfaces/forum/CommentService.java
@@ -0,0 +1,19 @@
+package com.uiu.thesis.services.interfaces.forum;
+
+import com.uiu.thesis.models.forum.Comment;
+import java.util.List;
+
+/**
+ *
+ * @author ashif
+ */
+public interface CommentService {
+
+    public int addNewComment(Comment comment);
+
+    public int editComment(Comment comment);
+
+    public List<Comment> getCommentsByPost(Long postId);
+
+    public Comment getCommentById(Long commentId);
+}
