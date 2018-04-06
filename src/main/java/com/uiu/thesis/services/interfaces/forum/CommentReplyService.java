3e6523a4627476718686fc657f7aa52571cb9092
ashif <ashif@bras>
2018-04-06 13:55:24 +0600

    ADDED: CommentReplyService.java interface with its necessary methods to provide CommentReply service.

---
+++ src/main/java/com/uiu/thesis/services/interfaces/forum/CommentReplyService.java
@@ -0,0 +1,17 @@
+package com.uiu.thesis.services.interfaces.forum;
+
+import com.uiu.thesis.models.forum.CommentReply;
+import java.util.List;
+
+/**
+ *
+ * @author ashif
+ */
+public interface CommentReplyService {
+
+    public int addNewCommentReply(CommentReply commentReply, Long replierId, Long commentId);
+
+    public int editCommentReply(CommentReply commentReply);
+
+    public List<CommentReply> getCommentReplysByComment(Long commentId);
+}
