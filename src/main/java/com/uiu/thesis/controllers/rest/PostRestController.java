6879a8f356a662d2871f951ec6e370e1b4610924
ashif <ashif@bras>
2018-04-08 04:00:53 +0600

    SERVICE IMPLEMENTED: getting posts by a user_id service is implemented. URL(/forum/post/get?user=102)

--- src/main/java/com/uiu/thesis/controllers/rest/PostRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/PostRestController.java
@@ -1,10 +1,13 @@
 package com.uiu.thesis.controllers.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.uiu.thesis.models.forum.Post;
 import com.uiu.thesis.services.interfaces.forum.PostService;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,16 +24,27 @@ public class PostRestController {
     @Autowired
     private PostService postService;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
 
+    /**
+     *
+     * @param postJson
+     * @param posterId
+     * @param tagJson
+     * @return
+     */
     @RequestMapping(
             value = "/forum/post/add",
             params = {"post", "poster", "tags"},
-            method = RequestMethod.POST)
-    public String testPostService(
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+
+    public String addNewPost(
             @RequestParam("post") String postJson,
             @RequestParam("poster") long posterId,
             @RequestParam("tags") String tagJson) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
 
         try {
 
@@ -46,5 +60,40 @@ public class PostRestController {
         }
 
         return "{\"insert\":\"true\"}";
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(
+            value = "/forum/post/get",
+            params = {"user"},
+            method = RequestMethod.GET,
+            produces = {"application/json;charset=UTF-8"})
+
+    public String getPosts(
+            @RequestParam("user") long id) {
+
+        List<Post> posts = postService.getPostsByUser(id);
+
+        String postsJson;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+
+            objectMapper.setDateFormat(df);
+            postsJson = objectMapper.writeValueAsString(posts);
+        } catch (JsonProcessingException ex) {
+
+            return "[]";
+        }
+
+        if (postsJson != null) {
+
+            return postsJson;
+        }
+
+        return "[]";
     }
 }
