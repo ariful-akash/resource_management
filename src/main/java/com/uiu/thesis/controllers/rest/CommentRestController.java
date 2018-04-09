206a1a300f194db130c42dc106b2ecd1506f033c
ashif <ashif@bras>
2018-04-10 02:35:21 +0600

    SERVICE API IMPLEMENTED: adding new comment service api is built. url_ :(/service/forum/comment/add) method_ :POST params_ : commentJson (Json string of Comment object) Example: /service/forum/comment/add?comment=commentJson

---
+++ src/main/java/com/uiu/thesis/controllers/rest/CommentRestController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uiu.thesis.controllers.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uiu.thesis.models.forum.Comment;
+import com.uiu.thesis.services.interfaces.forum.CommentService;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ashif
+ */
+@RestController(value = "/service/forum/comment")
+public class CommentRestController {
+
+    @Autowired
+    private CommentService commentService;
+
+    private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
+
+    /**
+     *
+     * @param commentJson
+     * @return
+     */
+    @RequestMapping(
+            value = "/service/forum/comment/add",
+            params = "comment",
+            produces = {"application/json;charset:UTF-8"},
+            method = RequestMethod.POST)
+    public String addCommentService(@RequestParam("comment") String commentJson) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+
+            Comment comment = objectMapper.readValue(commentJson, Comment.class);
+            if (comment != null
+                    && comment.getPostId() != null
+                    && comment.getCommenterId() != null
+                    && comment.getContent() != null
+                    && comment.getCommentTime() != null) {
+
+                int value = commentService.addNewComment(comment);
+
+                if (value != 0) {
+
+                    return "{\"insert\":\"true\"}";
+                }
+            }
+        } catch (IOException e) {
+
+            System.err.println(e.toString());
+        }
+
+        return "{\"insert\":\"false\"}";
+    }
+}
