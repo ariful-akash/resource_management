f2376cf067874034a41d1f21319a83fdf56e06b2
ashif <ashif@bras>
2018-04-09 12:59:31 +0600

    IMPLEMENTED: getCommentsByPost(Long postId) method is implemented in service layer.

--- src/main/java/com/uiu/thesis/services/implement/forum/CommentServiceImpl.java
+++ src/main/java/com/uiu/thesis/services/implement/forum/CommentServiceImpl.java
@@ -1,9 +1,11 @@
 package com.uiu.thesis.services.implement.forum;
 
+import com.uiu.thesis.dao.interfaces.CommentDAO;
 import com.uiu.thesis.models.forum.Comment;
 import com.uiu.thesis.services.interfaces.forum.CommentService;
 import java.util.List;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class CommentServiceImpl implements CommentService {
+
+    @Autowired
+    private CommentDAO commentDAO;
 
     @Override
     public int addNewComment(Comment comment, Long postId, Long commenterId) {
@@ -24,9 +29,20 @@ public class CommentServiceImpl implements CommentService {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *
+     * @param postId
+     * @return
+     */
     @Override
     public List<Comment> getCommentsByPost(Long postId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (postId > 0) {
+
+            return commentDAO.getAllComments(postId);
+        }
+
+        return null;
     }
 
     @Override
