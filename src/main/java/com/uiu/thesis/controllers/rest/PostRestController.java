3f6180e4cb61b20dc2f398de2bf4b04415c74d24
ashif <ashif@bras>
2018-04-09 16:07:10 +0600

    SERVICE IMPLEMENTED: getting comments of a post and its replys service is implemented. URL_ (/service/forum/post/comment/get) method_ (GET) parameter_ (postid) example: /service/forum/post/comment/get?postid=1004

--- src/main/java/com/uiu/thesis/controllers/rest/PostRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/PostRestController.java
@@ -3,10 +3,17 @@ package com.uiu.thesis.controllers.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.uiu.thesis.models.forum.Comment;
+import com.uiu.thesis.models.forum.CommentReply;
 import com.uiu.thesis.models.forum.Post;
+import com.uiu.thesis.models.forum.json.CommentsAndReplys;
+import com.uiu.thesis.models.forum.json.PostComments;
+import com.uiu.thesis.services.interfaces.forum.CommentReplyService;
+import com.uiu.thesis.services.interfaces.forum.CommentService;
 import com.uiu.thesis.services.interfaces.forum.PostService;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +30,12 @@ public class PostRestController {
 
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private CommentReplyService commentReplyService;
 
     private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
 
@@ -80,9 +93,9 @@ public class PostRestController {
 
         String postsJson;
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(df);
         try {
 
-            objectMapper.setDateFormat(df);
             postsJson = objectMapper.writeValueAsString(posts);
         } catch (JsonProcessingException ex) {
 
@@ -152,9 +165,9 @@ public class PostRestController {
 
             String postsJson;
             ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setDateFormat(df);
             try {
 
-                objectMapper.setDateFormat(df);
                 postsJson = objectMapper.writeValueAsString(posts);
             } catch (JsonProcessingException ex) {
 
@@ -169,4 +182,77 @@ public class PostRestController {
 
         return "[]";
     }
+
+    /**
+     *
+     * @param postId
+     * @return
+     */
+    @RequestMapping(
+            value = "/service/forum/post/comment/get",
+            params = "postid",
+            produces = {"application/json;charset=UTF-8"},
+            method = RequestMethod.GET)
+
+    public String getPostComments(@RequestParam("postid") long postId) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(df);
+        String postJsonString = "[]";
+
+        //Get the post by a specific ID
+        Post post = postService.getPostById(postId);
+
+        if (post != null) {
+
+            //Creating Post-Comments
+            PostComments postComments = new PostComments();
+            postComments.setId(post.getId());
+            postComments.setContent(post.getContent());
+            postComments.setPostTime(post.getPostTime());
+            postComments.setPosterId(post.getPosterId());
+            postComments.setTags(post.getTags());
+
+            //Get comments by the post ID
+            List<Comment> commentsList = commentService.getCommentsByPost(postId);
+
+            List<CommentsAndReplys> commentAndReplysList = new ArrayList<>();
+
+            for (Comment comment : commentsList) {
+
+                //Create comment and comment replys list
+                CommentsAndReplys commentAndReplys = new CommentsAndReplys();
+                commentAndReplys.setId(comment.getId());
+                commentAndReplys.setContent(comment.getContent());
+                commentAndReplys.setCommentTime(comment.getCommentTime());
+                commentAndReplys.setCommenterId(comment.getCommenterId());
+                commentAndReplys.setIsEdited(comment.isEdited());
+                commentAndReplys.setPostId(comment.getPostId());
+
+                List<CommentReply> replys
+                        = commentReplyService.getCommentReplysByComment(comment.getId());
+
+                //add reply list to a comment
+                commentAndReplys.setCommentReplys(replys);
+
+                //add comment and its replys to a list
+                commentAndReplysList.add(commentAndReplys);
+            }
+
+            //adding comments list of a post
+            postComments.setComments(commentAndReplysList);
+
+            //Creating JSON String of Post, Comments, and its replys
+            try {
+
+                postJsonString = objectMapper.writeValueAsString(postComments);
+            } catch (JsonProcessingException e) {
+
+                postJsonString = "[]";
+            }
+        }
+
+        return postJsonString;
+    }
+
 }
