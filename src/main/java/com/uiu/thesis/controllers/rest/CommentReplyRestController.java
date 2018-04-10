de9796dadf79a4c461a2f0bdc5fcd82454416d37
ashif <ashif@bras>
2018-04-10 13:11:42 +0600

    SERVICE API: to add new comment reply rest controller api is implemented. url_ (service/forum/reply/add) method_ POST params_ reply (an object of Commentreply Model)
    e.g.: /service/forum/reply/add?reply=replyJson

--- src/main/java/com/uiu/thesis/controllers/rest/CommentReplyRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/CommentReplyRestController.java
@@ -1,5 +1,13 @@
 package com.uiu.thesis.controllers.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uiu.thesis.models.forum.CommentReply;
+import com.uiu.thesis.services.interfaces.forum.CommentReplyService;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +17,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController(value = "/service/forum/reply")
 public class CommentReplyRestController {
 
+    @Autowired
+    private CommentReplyService commentReplyService;
+
+    /**
+     *
+     * @param replyJson
+     * @return
+     */
+    @RequestMapping(
+            value = "/service/forum/reply/add",
+            params = {"reply"},
+            produces = {"application/json;charset:UTF-8"},
+            method = RequestMethod.POST)
+    public String addNewReplyService(@RequestParam("reply") String replyJson) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+
+            CommentReply commentReply = objectMapper.readValue(replyJson, CommentReply.class);
+
+            if (commentReply != null
+                    && commentReply.getId() == null
+                    && commentReply.getReplierId() != null
+                    && commentReply.getCommentId() != null
+                    && commentReply.getDateTime() != null
+                    && commentReply.getReply() != null
+                    && commentReply.isEdited() == false) {
+
+                int value = commentReplyService.addNewCommentReply(commentReply);
+
+                if (value != 0) {
+
+                    return "{\"add\":\"true\"}";
+                }
+            }
+
+        } catch (IOException e) {
+
+            System.err.println(e.toString());
+        }
+
+        return "{\"add\":\"true\"}";
+    }
 }
