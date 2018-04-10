4532d0c2b2133dc2e28079912d376619d906e349
ashif <ashif@bras>
2018-04-10 12:24:08 +0600

    ADDED: CommentReplyRestController.java is added.

---
+++ src/main/java/com/uiu/thesis/controllers/rest/CommentReplyRestController.java
@@ -0,0 +1,12 @@
+package com.uiu.thesis.controllers.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ashif
+ */
+@RestController(value = "/service/forum/reply")
+public class CommentReplyRestController {
+
+}
