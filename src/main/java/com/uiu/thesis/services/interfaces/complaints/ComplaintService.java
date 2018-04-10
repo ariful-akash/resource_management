43a74db4ff7804b38bf715f3b2a074629f912a5a
ashif <ashif@bras>
2018-04-11 03:08:07 +0600

    ADDED: ComplaintService and ComplaintTypeService interface is added.

---
+++ src/main/java/com/uiu/thesis/services/interfaces/complaints/ComplaintService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uiu.thesis.services.interfaces.complaints;
+
+/**
+ *
+ * @author ashif
+ */
+public interface ComplaintService {
+
+}
