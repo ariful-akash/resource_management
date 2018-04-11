caecca8b28540bcb240d7c2bccb56096e97f090f
ashif <ashif@bras>
2018-04-11 16:06:03 +0600

    SERVICE IMPLEMENTED: get all complaints by its type is implemented in service layer. adding complaint is implemented but not tested.

--- src/main/java/com/uiu/thesis/services/implement/ComplaintServiceImpl.java
+++ src/main/java/com/uiu/thesis/services/implement/ComplaintServiceImpl.java
@@ -1,10 +1,12 @@
 package com.uiu.thesis.services.implement;
 
+import com.uiu.thesis.dao.interfaces.ComplaintDAO;
 import com.uiu.thesis.models.complaint.Complaint;
 import com.uiu.thesis.services.interfaces.ComplaintService;
 import java.util.Date;
 import java.util.List;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,14 +17,43 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class ComplaintServiceImpl implements ComplaintService {
 
+    @Autowired
+    private ComplaintDAO complaintDAO;
+
+    /**
+     *
+     * @param complaint
+     * @return
+     */
     @Override
     public int addNewComplaint(Complaint complaint) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (complaint != null
+                && complaint.getId() == null
+                && complaint.getComplaintPlacingDate() != null
+                && complaint.getCreatorId() != null
+                && complaint.getDescription() != null
+                && complaint.getType() != null) {
+
+            return complaintDAO.addComplaint(complaint);
+        }
+        return 0;
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @Override
     public List<Complaint> getComplaintsByType(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (id > 0) {
+
+            return complaintDAO.getComplaintsByType(id);
+        }
+
+        return null;
     }
 
     @Override
