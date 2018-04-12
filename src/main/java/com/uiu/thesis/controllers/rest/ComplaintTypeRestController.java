58808a56a9d971fb57f4e5666ddde3ff4215855f
ashif <ashif@bras>
2018-04-12 15:56:57 +0600

    SERVICE API IMPL: api is implemented to insert a new complaint type.
    url_ (/api/service/office/complainttype)
    method_ POST
    params_ complaint_type

--- src/main/java/com/uiu/thesis/controllers/rest/ComplaintTypeRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/ComplaintTypeRestController.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uiu.thesis.models.complaint.ComplaintType;
 import com.uiu.thesis.services.interfaces.ComplaintTypeService;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -50,5 +52,39 @@ public class ComplaintTypeRestController {
             }
         }
         return "[]";
+    }
+
+    /**
+     * Add a new complaint type
+     *
+     * @param complaintTypeJson
+     * @return
+     */
+    @RequestMapping(
+            value = "/api/service/office/complainttype",
+            produces = {"application/json;charset;UTF-8"},
+            params = {"complaint_type"},
+            method = RequestMethod.POST)
+    public String addComplaint(
+            @RequestParam("complaint_type") String complaintTypeJson) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+
+            ComplaintType complaintType = objectMapper.readValue(complaintTypeJson, ComplaintType.class);
+
+            int value = complaintTypeService.addComplaint(complaintType);
+            if (value != 0) {
+
+                return "{\"add\":\"true\"}";
+            }
+
+        } catch (IOException e) {
+
+            System.err.println(e.toString());
+        }
+
+        return "{\"add\":\"false\"}";
     }
 }
