6ded87730a03bc75369d05c2da6c6596db05051f
ashif <ashif@bras>
2018-04-12 23:34:19 +0600

    SERVICE API IMPL: service api is implemented to add new requisition type in database

--- src/main/java/com/uiu/thesis/controllers/rest/RequisitionTypeRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/RequisitionTypeRestController.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uiu.thesis.models.requisition.RequisitionType;
 import com.uiu.thesis.services.interfaces.RequisitionTypeService;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +26,7 @@ public class RequisitionTypeRestController {
     private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
 
     /**
+     * Returns all the requisitions
      *
      * @return
      */
@@ -52,5 +55,42 @@ public class RequisitionTypeRestController {
         }
 
         return "[]";
+    }
+
+    /**
+     * Add a new requisition type
+     *
+     * @param requisitionTypeJson
+     * @return
+     */
+    @RequestMapping(
+            value = "/api/service/office/requisitiontype",
+            params = {"requisition_type"},
+            produces = {"application/json;charset:UTF-8"},
+            method = RequestMethod.POST)
+    public String addRequisition(
+            @RequestParam("requisition_type") String requisitionTypeJson) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (requisitionTypeJson != null) {
+
+            try {
+
+                RequisitionType requisitionType
+                        = objectMapper.readValue(requisitionTypeJson, RequisitionType.class);
+
+                int value = requisitionTypeService.addRequisitionType(requisitionType);
+                if (value != 0) {
+
+                    return "{\"add\":\"true\"}";
+                }
+
+            } catch (IOException ex) {
+
+                System.err.println(ex.toString());
+            }
+        }
+
+        return "{\"add\":\"false\"}";
     }
 }
