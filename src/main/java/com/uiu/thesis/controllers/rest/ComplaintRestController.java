ac0734045c5577813abf0f9597129b036c7381ca
ashif <ashif@bras>
2018-04-12 12:32:29 +0600

    RENAME: Controller class is renamed.

--- src/main/java/com/uiu/thesis/controllers/rest/ComplaintRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/ComplaintRestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ashif
  */
 @RestController(value = "/api/service/office/complaint")
-public class ComlaintRestController {
+public class ComplaintRestController {
 
     @Autowired
     private ComplaintService complaintService;
