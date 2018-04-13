bc43452c2eebe228cdd7d5655f84a3a9404aff87
ashif <ashif@bras>
2018-04-14 00:55:32 +0600

    MODEL MOD: @JsonIgnore added with password in Human resource model's field

--- src/main/java/com/uiu/thesis/models/user/HumanResource.java
+++ src/main/java/com/uiu/thesis/models/user/HumanResource.java
@@ -1,6 +1,7 @@
 package com.uiu.thesis.models.user;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -43,6 +44,7 @@ public class HumanResource implements Serializable {
 
     @Lob
     @Column(name = "password", nullable = false)
+    @JsonIgnore
     private byte[] password;
 
     @Column(name = "phone", nullable = true)
