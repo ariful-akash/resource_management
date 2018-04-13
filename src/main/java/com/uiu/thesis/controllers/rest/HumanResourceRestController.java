a2da472aa98b7e57c247a7bbb0bb5d13039e03f5
ashif <ashif@bras>
2018-04-14 02:56:38 +0600

    SSRVICE API IMPL: api is implemented to get human resources by type/access/role.
    url_ /api/service/office/hr/{key}/{id}
    method_ GET
    params_ n/a
    
    key: type
    	 role
    	 access
    id: id of the corresponding key.

--- src/main/java/com/uiu/thesis/controllers/rest/HumanResourceRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/HumanResourceRestController.java
@@ -7,6 +7,7 @@ import com.uiu.thesis.services.interfaces.HumanResourceService;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,62 @@ public class HumanResourceRestController {
                 System.err.println(ex.toString());
             }
         }
+        return "[]";
+    }
+
+    /**
+     *
+     * @param key
+     * @param id
+     * @return
+     */
+    @RequestMapping(
+            value = "/api/service/office/hr/{key}/{id}",
+            produces = {"application/json;charset:UTF-8"},
+            method = RequestMethod.GET)
+    public String getHumanResources(
+            @PathVariable("key") String key,
+            @PathVariable("id") long id) {
+
+        if (key != null && !key.isEmpty() && id > 0) {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            List<HumanResource> humanResources = null;
+
+            switch (key) {
+
+                case "type":
+
+                    humanResources = humanResourceService.getHumanResourcesByType(id);
+                    break;
+
+                case "role":
+
+                    humanResources = humanResourceService.getHumanResourcesByRole(id);
+                    break;
+
+                case "access":
+
+                    humanResources = humanResourceService.getHumanResourcesByAccess(id);
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (humanResources != null && humanResources.size() > 0) {
+
+                try {
+
+                    return objectMapper.writeValueAsString(humanResources);
+                } catch (JsonProcessingException e) {
+
+                    System.err.println(e.toString());
+                }
+            }
+        }
+
         return "[]";
     }
 }
