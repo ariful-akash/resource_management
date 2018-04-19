875bafb52e0defd123d76ba9be7a191d9112a1e2
ashif <ashif@bras>
2018-04-20 01:10:03 +0600

    TAG REST MOD: token session verified before service providing.

--- src/main/java/com/uiu/thesis/controllers/rest/TagTypeRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/TagTypeRestController.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.uiu.thesis.controllers.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uiu.thesis.dao.interfaces.TagTypeDAO;
+import com.uiu.thesis.dao.interfaces.TokenDAO;
 import com.uiu.thesis.models.forum.TagType;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,24 +22,38 @@ public class TagTypeRestController {
     @Autowired
     private TagTypeDAO tagTypeDAO;
 
+    @Autowired
+    private TokenDAO tokenDAO;
+
+    /**
+     * Return all the tags
+     *
+     * @param session
+     * @return
+     */
     @RequestMapping(
             value = "/api/service/forum/tag/tags",
             method = RequestMethod.GET,
             produces = {"application/json;charset:UTF-8"})
-    public String getAllTagService() {
+    public String getAllTagService(HttpSession session) {
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        String token = (String) session.getAttribute("token");
 
-        List<TagType> tagTypesList = tagTypeDAO.getAllTagTypes();
+        if (token != null && tokenDAO.isTokenExist(token)) {
 
-        if (tagTypesList != null && tagTypesList.size() > 0) {
+            ObjectMapper objectMapper = new ObjectMapper();
 
-            try {
+            List<TagType> tagTypesList = tagTypeDAO.getAllTagTypes();
 
-                return objectMapper.writeValueAsString(tagTypesList);
-            } catch (JsonProcessingException ex) {
+            if (tagTypesList != null && tagTypesList.size() > 0) {
 
-                System.err.println(ex.toString());
+                try {
+
+                    return objectMapper.writeValueAsString(tagTypesList);
+                } catch (JsonProcessingException ex) {
+
+                    System.err.println(ex.toString());
+                }
             }
         }
 
