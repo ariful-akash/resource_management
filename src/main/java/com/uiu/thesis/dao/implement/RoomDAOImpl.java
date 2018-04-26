a6414c37b66f7f69d5055fcebfbb121094f88a54
ashif <ashif@bras>
2018-04-26 13:37:20 +0600

    DAO IMPL: getRoomByFloor is implemented.

--- src/main/java/com/uiu/thesis/dao/implement/RoomDAOImpl.java
+++ src/main/java/com/uiu/thesis/dao/implement/RoomDAOImpl.java
@@ -8,8 +8,13 @@ package com.uiu.thesis.dao.implement;
 import com.uiu.thesis.dao.interfaces.RoomDAO;
 import com.uiu.thesis.models.object_resource.Floor;
 import com.uiu.thesis.models.object_resource.Room;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -83,9 +88,23 @@ public class RoomDAOImpl implements RoomDAO {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *
+     * @param floorId
+     * @return
+     */
     @Override
     public List<Room> getRoomsByFloor(Long floorId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (floorId != null && floorId > 0) {
+
+            String sql = "SELECT * FROM room"
+                    + " WHERE floor_id = " + floorId;
+
+            return getRoomsBySql(sql);
+        }
+
+        return null;
     }
 
     /**
@@ -105,4 +124,40 @@ public class RoomDAOImpl implements RoomDAO {
         return null;
     }
 
+    /**
+     *
+     * @param sql
+     * @return
+     */
+    private List<Room> getRoomsBySql(String sql) {
+
+        if (sql != null && !sql.isEmpty()) {
+
+            Session session = sessionFactory.getCurrentSession();
+
+            Query query = session.createSQLQuery(sql);
+            query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+            List results = query.list();
+
+            if (results != null && results.size() > 0) {
+
+                List<Room> rooms = new ArrayList<>();
+
+                for (Object result : results) {
+
+                    Room room = new Room();
+                    Map row = (Map) result;
+
+                    BigInteger idInt = (BigInteger) row.get("id");
+
+                    room.setId(idInt.longValue());
+                    room.setRoom((String) row.get("room"));
+
+                    rooms.add(room);
+                }
+            }
+        }
+
+        return null;
+    }
 }
