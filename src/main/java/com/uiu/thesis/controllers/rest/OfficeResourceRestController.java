5f1453b40e65b3d5479d72cc309dcbddec253641
ashif <ashif@bras>
2018-04-28 15:27:33 +0600

    API implemented to add and read office resources.

--- src/main/java/com/uiu/thesis/controllers/rest/OfficeResourceRestController.java
+++ src/main/java/com/uiu/thesis/controllers/rest/OfficeResourceRestController.java
@@ -2,10 +2,14 @@ package com.uiu.thesis.controllers.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uiu.thesis.dao.interfaces.FloorDAO;
 import com.uiu.thesis.dao.interfaces.OfficeResourceDAO;
 import com.uiu.thesis.dao.interfaces.OfficeResourceTypeDAO;
 import com.uiu.thesis.dao.interfaces.TokenDAO;
 import com.uiu.thesis.models.forum.json.OfficeResourceJson;
+import com.uiu.thesis.models.forum.json.ResourceByFloorJson;
+import com.uiu.thesis.models.forum.json.ResourceByRoomJson;
+import com.uiu.thesis.models.object_resource.Floor;
 import com.uiu.thesis.models.object_resource.OfficeResource;
 import com.uiu.thesis.models.object_resource.OfficeResourceType;
 import java.text.SimpleDateFormat;
@@ -34,6 +38,9 @@ public class OfficeResourceRestController {
 
     @Autowired
     private TokenDAO tokenDAO;
+
+    @Autowired
+    private FloorDAO floorDAO;
 
     private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
 
@@ -271,6 +278,80 @@ public class OfficeResourceRestController {
 
     /**
      *
+     * @param type
+     * @param floor
+     * @param room
+     * @param session
+     * @return
+     */
+    @RequestMapping(
+            value = "/api/service/office/officeresource/specific",
+            params = {"type", "floor", "room"},
+            produces = {"application/json;charset:UTF-8"},
+            method = RequestMethod.GET)
+    public String getSpecificResources(
+            @RequestParam("type") String type,
+            @RequestParam("floor") String floor,
+            @RequestParam("room") String room,
+            HttpSession session) {
+
+        String token = (String) session.getAttribute("token");
+        if (token != null && tokenDAO.isTokenExist(token)) {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            if (type != null && !type.trim().isEmpty()
+                    && floor != null && room != null) {
+
+                OfficeResourceType resourceType = officeResourceTypeDAO.getOfficeResourceType(type);
+                if (resourceType != null) {
+
+                    List<OfficeResource> officeResources;
+                    List<ResourceByFloorJson> resourceByFloorJsons = new ArrayList<>();
+
+                    if (floor.equals("All")) {
+
+                        List<Floor> floors = floorDAO.getAllFloors();
+
+                        for (Floor floor1 : floors) {
+
+                            officeResources = officeResourceDAO.getOfficeResources(resourceType.getId(), floor1.getFloor());
+
+                            resourceByFloorJsons.add(getResourceByFloorJson(floor1, officeResources));
+                        }
+                    } else if (room.equals("All")) {
+
+                        Floor floor1 = floorDAO.getFloor(floor);
+                        officeResources = officeResourceDAO.getOfficeResources(resourceType.getId(), floor);
+
+                        resourceByFloorJsons.add(getResourceByFloorJson(floor1, officeResources));
+                    } else {
+
+                        Floor floor1 = floorDAO.getFloor(floor);
+                        officeResources = officeResourceDAO.getOfficeResources(resourceType.getId(), floor, room);
+
+                        resourceByFloorJsons.add(getResourceByFloorJson(floor1, officeResources));
+                    }
+
+                    if (resourceByFloorJsons.size() > 0) {
+
+                        try {
+
+                            return objectMapper.writeValueAsString(resourceByFloorJsons);
+                        } catch (JsonProcessingException e) {
+
+                            System.err.println(e.toString());
+                        }
+                    }
+                }
+            }
+        }
+
+        return "[]";
+    }
+
+    /**
+     *
      * @param resourceType
      * @return
      */
@@ -289,5 +370,40 @@ public class OfficeResourceRestController {
                 officeResourceDAO.getOfficeResourcesByType(resourceType.getId()));
 
         return officeResourceJson;
+    }
+
+    /**
+     *
+     * @param floor
+     * @param officeResource
+     * @return
+     */
+    private ResourceByFloorJson getResourceByFloorJson(Floor floor, List<OfficeResource> officeResources) {
+
+        if (floor == null || officeResources == null || officeResources.size() <= 0) {
+
+            return null;
+        }
+
+        ResourceByFloorJson resourceByFloorJson = new ResourceByFloorJson();
+
+        resourceByFloorJson.setId(floor.getId());
+        resourceByFloorJson.setFloor(floor.getFloor());
+
+        List<ResourceByRoomJson> byRoomJsons = new ArrayList<>();
+
+        for (OfficeResource officeResource : officeResources) {
+
+            ResourceByRoomJson resourceByRoomJson = new ResourceByRoomJson();
+
+            resourceByRoomJson.setRoom(officeResource.getRoom());
+            resourceByRoomJson.setQuantity(officeResource.getQuantity());
+
+            byRoomJsons.add(resourceByRoomJson);
+        }
+
+        resourceByFloorJson.setRooms(byRoomJsons);
+
+        return resourceByFloorJson;
     }
 }
